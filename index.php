949f11c7d2abdd6e3255869408d280a39094a28e
Lashawn van der Graaf <99045686@mydavinci.nl>
2018-02-07 09:40:04 +0100

    Goede morgen opdracht

--- index.php
+++ index.php
@@ -1,10 +1,55 @@
+<?php
+	date_default_timezone_set('CET');
+	$hour = date("h:i");
+
+
+	if ($hour < 6){
+		$class = "nacht";
+		$bg = "backgrounds/night.png";
+	}
+	if ($hour >=6 && $hour <12) {
+		$class = "morgen";
+		$bg = "backgrounds/morning.png";
+	}
+	if ($hour >=12 && $hour <18) {
+		$class = "middag";
+		$bg = "backgrounds/afternoon.png";
+	}
+	if ($hour >= 18) {
+		$class = "avond";
+		$bg = "evening/afternoon.png";
+	}
+?>
+
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>Het is nu: <?=$hour?></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta charset="utf-8">
+
 </head>
-<body>
-	
+<body background="<?=$bg?>" class="<?=$class?>">
+<h1>Goede<?=$class?></h1>
+<h1>Het is nu <?=$hour?></h1>
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
